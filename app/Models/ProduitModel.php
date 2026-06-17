100fe5cac7cee828de9af1e87db2e0d956fb25e8
NyAntema <216714695+mitiela14@users.noreply.github.com>
2026-06-17 09:45:27 +0300

    add

---
+++ app/Models/ProduitModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+
+class ProduitModel extends Model 
+{
+    protected $table = 'produits';
+
+    protected $primaryKey = 'id';
+
+    protected 
+}
